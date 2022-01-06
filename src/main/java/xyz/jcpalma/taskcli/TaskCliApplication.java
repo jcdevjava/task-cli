c6888a3064f84aadd3a45f7cca5c215ad7c2a340
José Carlos Palma <palmahn@gmail.com>
2022-01-05 21:48:46 -0600

    init(app): Creación del proyecto task-cli.

---
+++ src/main/java/xyz/jcpalma/taskcli/TaskCliApplication.java
@@ -0,0 +1,13 @@
+package xyz.jcpalma.taskcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskCliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/jcpalma/taskcli/TaskCliApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.jcpalma.taskcli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
